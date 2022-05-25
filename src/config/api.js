4ff946053907b4d7a9befcd71d037ba496e508a8
IIKNBNSCOE <imran.kazi90@gmail.com>
2022-05-25 10:08:07 +0530

    backed ip address modified

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,3 @@
-export  const LoginURL="http://18.132.40.163:7000/user/login"
-export const RegisterURL="http://18.132.40.163:7000/user/register"
-export const TicketURL="http://18.132.40.163:7000/ticket"
-export  const TokenVerifyURL="http://18.132.40.163:7000/user/verify"
+export const RegisterURL="http://18.170.69.246:7000/user/register"
+export const TicketURL="http://18.170.69.246:7000/ticket"
+export  const TokenVerifyURL="http://18.170.69.246:7000/user/verify"
