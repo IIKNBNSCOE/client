011bbd7409be32579a0bee04d7521d255c512c7e
IIKNBNSCOE <imran.kazi90@gmail.com>
2022-05-25 11:33:46 +0530

    login url updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,4 @@
 export const RegisterURL="http://18.170.69.246:7000/user/register"
+export const LoginURL="http://18.170.69.246:7000/user/login"
 export const TicketURL="http://18.170.69.246:7000/ticket"
 export  const TokenVerifyURL="http://18.170.69.246:7000/user/verify"
