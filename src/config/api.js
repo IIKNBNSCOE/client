c0c0d98706df55404d56e42a31758b5f37463944
IIKNBNSCOE <imran.kazi90@gmail.com>
2022-07-11 16:27:30 +0530

    server address updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const RegisterURL="http://18.170.69.246:7000/user/register"
-export const LoginURL="http://18.170.69.246:7000/user/login"
-export const TicketURL="http://18.170.69.246:7000/ticket"
-export  const TokenVerifyURL="http://18.170.69.246:7000/user/verify"
+export const RegisterURL="http://35.176.37.30:7000/user/register"
+export const LoginURL="http://35.176.37.30:7000/user/login"
+export const TicketURL="http://35.176.37.30:7000/ticket"
+export  const TokenVerifyURL="http://35.176.37.30:7000/user/verify"
